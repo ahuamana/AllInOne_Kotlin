d5e7f05c41c9fe0c1387af431780957608e8e4ff
Alikhan <73962071@continental.edu.pe>
2021-07-27 11:03:52 -0500

    send context to view model from fragment to work everthing on viewmodel

--- app/src/main/java/com/paparazziteam/allinone/adapters/NewsAdapter.kt
+++ app/src/main/java/com/paparazziteam/allinone/adapters/NewsAdapter.kt
@@ -1,8 +1,13 @@
 package com.paparazziteam.allinone.adapters
 
 
+import android.content.Context
+import android.provider.Settings.Global.getString
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import com.paparazziteam.allinone.databinding.CardviewNewsBinding
 import com.paparazziteam.allinone.models.NewsItem
@@ -10,8 +15,12 @@ import com.paparazziteam.allinone.viewModels.NewsItemViewModel
 
 class NewsAdapter (private val newsList: List<NewsItem>) : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>(){
 
+    lateinit var context: Context
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         val binding = CardviewNewsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        context = parent.context
 
         return NewsViewHolder(binding)
 
@@ -20,6 +29,7 @@ class NewsAdapter (private val newsList: List<NewsItem>) : RecyclerView.Adapter<
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         val newsItem = newsList[position]
         holder.bind(newsItem)
+
     }
 
     override fun getItemCount() = newsList.size
@@ -30,15 +40,20 @@ class NewsAdapter (private val newsList: List<NewsItem>) : RecyclerView.Adapter<
     inner class NewsViewHolder(private  val binding: CardviewNewsBinding) : RecyclerView.ViewHolder(binding.root)
     {
         init {
-            binding.viewModel = NewsItemViewModel()
+            binding.viewModel = NewsItemViewModel(context)
         }
 
         fun bind(newsItem: NewsItem){
             binding.apply {
                 viewModel?.setNewsItem(newsItem)
+
                 executePendingBindings()
             }
         }
+
     }
+
+
+
 
 }
